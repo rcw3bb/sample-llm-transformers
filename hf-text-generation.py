64a841bacb0660960e9831956136c69f2dbbadf1
Ron Webb <ron@ronella.xyz>
2025-02-03 00:39:01 +1300

    Lessen the generation of truncated statements.

--- hf-text-generation.py
+++ hf-text-generation.py
@@ -1,13 +1,16 @@
-import transformers
 from transformers import pipeline
 
 
 def main():
     text_generator = pipeline("text-generation", model="gpt2")
-    # Set seed for reproducibility
-    transformers.set_seed(1)
     prompt = "AI is the future of technology because"
-    synthetic_text = text_generator(prompt, num_return_sequences=5, max_new_tokens=50)
+
+    synthetic_text = text_generator(prompt, num_return_sequences=5, # Generate 5 different completions
+                                    max_new_tokens=500, # Generate up to 500 tokens
+                                    temperature=0.2, # Low temperature for more deterministic results
+                                    repetition_penalty=1.2, # Penalize repeated text
+                                    )
+
     for text in synthetic_text:
         print(text.get("generated_text"), "\n---------------")
 
