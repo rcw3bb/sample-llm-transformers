f26ed1d049d95cb6f094e6fa4ae4c991b23e3ef4
Ron Webb <ron@ronella.xyz>
2025-02-03 00:20:42 +1300

    Add format the summary with the proper capilization.

--- hf-text-summarization.py
+++ hf-text-summarization.py
@@ -1,6 +1,28 @@
 from transformers import pipeline
+import truecase
+import nltk
+
+
+def check_and_download_nltk_data(dataset_name):
+    """
+    Check if the NLTK dataset is downloaded and download it if it is not
+    :param dataset_name: The name of the dataset
+    :return: None
+    """
+    try:
+        # Try to use the dataset
+        nltk.data.find(f'tokenizers/{dataset_name}')
+        print(f"The '{dataset_name}' dataset is already downloaded.")
+    except LookupError:
+        # If the dataset is not found, download it
+        print(f"The '{dataset_name}' dataset is not found. Downloading it now...")
+        nltk.download(dataset_name)
 
 def main() :
+
+    # Check if the 'punkt' dataset is downloaded
+    check_and_download_nltk_data('punkt_tab')
+
     summarizer = pipeline("summarization", model=model)
 
     text = """This is a long text that we want to summarize. 
@@ -12,7 +34,7 @@ def main() :
     summary = summarizer(text)
 
     print("Text: ", text)
-    print("Summary: ", summary[0]['summary_text'])
+    print("Summary: ", truecase.get_true_case(summary[0]['summary_text']))
 
 if __name__ == "__main__":
     model = "google-t5/t5-small"
